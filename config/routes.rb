796a08164883b3cd6e2f9943a66cccc7a00ab70e
tiggy31 <tigist_alemu@fitnyc.edu>
2020-07-20 12:33:42 -0400

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  resources :listings
+  resources :neighborhoods
+  resources :reviews
+  resources :reservations
   resources :users
 
-  resources :listings
 
-  resources :neighborhoods
 
   resources :cities
 
